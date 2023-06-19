e30b33484292b6a7c726c4e1690fa7b62f931b9d
Jacob McSwain <jacob@mcswain.dev>
2023-06-18 19:44:07 -0500

    move db under data

--- src/db.py
+++ src/db.py
@@ -7,6 +7,6 @@ _db = None
 def get_engine():
     global _db
     if _db is None:
-        _db = create_engine('sqlite+pysqlite:///wx.db', echo=True)
+        _db = create_engine('sqlite+pysqlite:///data/wx.db', echo=True)
         init_db(_db)
     return _db
