a1e4e2ce950c4e8e56e7762ecba4b34c24d8ee2b
Jacob McSwain <jacob@mcswain.dev>
2023-06-20 20:17:30 -0500

    alert: use color code

--- src/alert.py
+++ src/alert.py
@@ -16,11 +16,252 @@ from sqlalchemy.orm import Session
 class WXAlert():
     def plot(self, ax):
         if type(self.polygon) == Polygon:
-            ax.plot(*self.polygon.exterior.xy, color='red', linewidth=0.5, zorder=1)
+            ax.plot(*self.polygon.exterior.xy, color=self._get_color(), linewidth=3, zorder=6)
         elif type(self.polygon) == MultiPolygon:
             for geom in self.polygon.geoms:
                 xs, ys = geom.exterior.xy
-                ax.fill(xs, ys, color='red', linewidth=0.5, zorder=1)
+                ax.fill(xs, ys, color=self._get_color(), linewidth=0.5, zorder=1)
+
+    # Event codes: https://www.weather.gov/nwr/eventcodes
+    # https://www.weather.gov/help-map
+    def _get_color(self):
+        if self.event == "Blizzard Warning":
+            return "#FF4500"
+        elif self.event == "Coastal Flood Watch":
+            return "#66CDAA"
+        elif self.event == "Coastal Flood Warning":
+            return "#228B22"
+        elif self.event == "Dust Storm Warning":
+            return "#FFE4C4"
+        elif self.event == "Extreme Wind Warning":
+            return "#FF8C00"
+        elif self.event == "Flash Flood Watch":
+            return "#2E8B57"
+        elif self.event == "Flash Flood Warning":
+            return "#8B0000"
+        elif self.event == "Flash Flood Statement":
+            return "#8B0000"
+        elif self.event == "Flood Watch":
+            return "#2E8B57"
+        elif self.event == "Flood Warning":
+            return "#00FF00"
+        elif self.event == "Flood Statement":
+            return "#00FF00"
+        elif self.event == "High Wind Watch":
+            return "#B8860B"
+        elif self.event == "High Wind Warning":
+            return "#DAA520"
+        elif self.event == "Hurricane Watch":
+            return "#FF00FF"
+        elif self.event == "Hurricane Warning":
+            return "#DC143Cs"
+        elif self.event == "Hurricane Statement":
+            return "#FFE4B5"
+        elif self.event == "Severe Thunderstorm Watch":
+            return "#DB7093"
+        elif self.event == "Severe Thunderstorm Warning":
+            return "#FFA500"
+        elif self.event == "Severe Weather Statement":
+            return "#00FFFF"
+        elif self.event == "Snow Squall Warning":
+            return "#C71585"
+        elif self.event == "Special Marine Warning":
+            return "#FFA500"
+        elif self.event == "Special Weather Statement":
+            return "#FFE4B5"
+        elif self.event == "Storm Surge Watch":
+            return "#DB7FF7"
+        elif self.event == "Storm Surge Warning":
+            return "#B524F7"
+        elif self.event == "Tornado Watch":
+            return "#FFFF00"
+        elif self.event == "Tornado Warning":
+            return "#FF0000"
+        elif self.event == "Tropical Storm Watch":
+            return "#F08080"
+        elif self.event == "Tropical Storm Warning":
+            return "#B22222"
+        elif self.event == "Tsunami Watch":
+            return "#FF00FF"
+        elif self.event == "Tsunami Warning":
+            return "#FD6347"
+        elif self.event == "Winter Storm Watch":
+            return "#4682B4"
+        elif self.event == "Winter Storm Warning":
+            return "#FF69B4"
+        elif self.event == "Avalanche Watch":
+            return "#F4A460"
+        elif self.event == "Avalanche Warning":
+            return "#1E90FF"
+        elif self.event == "Blue Alert":
+            return "#B0C4DE"
+        elif self.event == "Child Abduction Emergency":
+            return "#800000"
+        elif self.event == "Civil Danger Warning":
+            return "#FFB6C1"
+        elif self.event == "Civil Emergency Message":
+            return "#FFB6C1"
+        elif self.event == "Earthquake Warning":
+            return "#8B4513"
+        elif self.event == "Evacuation Immediate":
+            return "#7FFF00"
+        elif self.event == "Fire Warning":
+            return "#A0522D"
+        elif self.event == "Hazardous Materials Warning":
+            return "#4B0082"
+        elif self.event == "Law Enforcement Warning":
+            return "#C0C0C0"
+        elif self.event == "Local Area Emergency":
+            return "#C0C0C0"
+        elif self.event == "911 Telephone Outage Emergency":
+            return "#C0C0C0"
+        elif self.event == "Nuclear Power Plant Warning":
+            return "#4B0082"
+        elif self.event == "Radiological Hazard Warning":
+            return "#4B0082"
+        elif self.event == "Shelter in Place Warning":
+            return "#FA8072"
+        elif self.event == "Volcano Warning":
+            return "#2F4F4F"
+        else:
+            return "#FD6347"
+
+    def should_show_radar(self):
+        isWx = self.is_weather()
+        if isWx and self.event == "Blizzard Warning":
+            return True
+        elif isWx and self.event == "Coastal Flood Watch":
+            return False
+        elif isWx and self.event == "Coastal Flood Warning":
+            return True
+        elif isWx and self.event == "Dust Storm Warning":
+            return True
+        elif isWx and self.event == "Extreme Wind Warning":
+            return False
+        elif isWx and self.event == "Flash Flood Watch":
+            return False
+        elif isWx and self.event == "Flash Flood Warning":
+            return True
+        elif isWx and self.event == "Flash Flood Statement":
+            return True
+        elif isWx and self.event == "Flood Watch":
+            return False
+        elif isWx and self.event == "Flood Warning":
+            return True
+        elif isWx and self.event == "Flood Statement":
+            return True
+        elif isWx and self.event == "High Wind Watch":
+            return False
+        elif isWx and self.event == "High Wind Warning":
+            return False
+        elif isWx and self.event == "Hurricane Watch":
+            return False
+        elif isWx and self.event == "Hurricane Warning":
+            return True
+        elif isWx and self.event == "Hurricane Statement":
+            return True
+        elif isWx and self.event == "Severe Thunderstorm Watch":
+            return False
+        elif isWx and self.event == "Severe Thunderstorm Warning":
+            return True
+        elif isWx and self.event == "Severe Weather Statement":
+            return True
+        elif isWx and self.event == "Snow Squall Warning":
+            return True
+        elif isWx and self.event == "Special Marine Warning":
+            return True
+        elif isWx and self.event == "Special Weather Statement":
+            return True
+        elif isWx and self.event == "Storm Surge Watch":
+            return False
+        elif isWx and self.event == "Storm Surge Warning":
+            return True
+        elif isWx and self.event == "Tornado Watch":
+            return False
+        elif isWx and self.event == "Tornado Warning":
+            return True
+        elif isWx and self.event == "Tropical Storm Watch":
+            return False
+        elif isWx and self.event == "Tropical Storm Warning":
+            return True
+        elif isWx and self.event == "Tsunami Watch":
+            return False
+        elif isWx and self.event == "Tsunami Warning":
+            return False
+        elif isWx and self.event == "Winter Storm Watch":
+            return False
+        elif isWx and self.event == "Winter Storm Warning":
+            return True
+        else:
+            return False
+
+    def is_weather(self):
+        if self.event == "Blizzard Warning":
+            return True
+        elif self.event == "Coastal Flood Watch":
+            return True
+        elif self.event == "Coastal Flood Warning":
+            return True
+        elif self.event == "Dust Storm Warning":
+            return True
+        elif self.event == "Extreme Wind Warning":
+            return True
+        elif self.event == "Flash Flood Watch":
+            return True
+        elif self.event == "Flash Flood Warning":
+            return True
+        elif self.event == "Flash Flood Statement":
+            return True
+        elif self.event == "Flood Watch":
+            return True
+        elif self.event == "Flood Warning":
+            return True
+        elif self.event == "Flood Statement":
+            return True
+        elif self.event == "High Wind Watch":
+            return True
+        elif self.event == "High Wind Warning":
+            return True
+        elif self.event == "Hurricane Watch":
+            return True
+        elif self.event == "Hurricane Warning":
+            return True
+        elif self.event == "Hurricane Statement":
+            return True
+        elif self.event == "Severe Thunderstorm Watch":
+            return True
+        elif self.event == "Severe Thunderstorm Warning":
+            return True
+        elif self.event == "Severe Weather Statement":
+            return True
+        elif self.event == "Snow Squall Warning":
+            return True
+        elif self.event == "Special Marine Warning":
+            return True
+        elif self.event == "Special Weather Statement":
+            return True
+        elif self.event == "Storm Surge Watch":
+            return True
+        elif self.event == "Storm Surge Warning":
+            return True
+        elif self.event == "Tornado Watch":
+            return True
+        elif self.event == "Tornado Warning":
+            return True
+        elif self.event == "Tropical Storm Watch":
+            return True
+        elif self.event == "Tropical Storm Warning":
+            return True
+        elif self.event == "Tsunami Watch":
+            return True
+        elif self.event == "Tsunami Warning":
+            return True
+        elif self.event == "Winter Storm Watch":
+            return True
+        elif self.event == "Winter Storm Warning":
+            return True
+        else:
+            return False
 
     def __init__(self, feature_json, state):
         self._feature_json = feature_json
