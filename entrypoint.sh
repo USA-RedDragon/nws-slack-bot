86ef8939f7ac8548255430824b1ee4c495df4a58
Jacob McSwain <jacob@mcswain.dev>
2023-06-21 15:11:49 -0500

    entrypoint: fixup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-trap "kill -TERM -$$" SIGINT SIGTERM EXIT
+set -xe
+
+cd /app
 
 # Check env for CONFIG_JSON
 if [ -z "$CONFIG_JSON" ]; then
@@ -11,6 +13,7 @@ fi
 # Put CONFIG_JSON into config.json
 echo -e $CONFIG_JSON > /app/config.json
 
-gunicorn --worker-tmp-dir /dev/shm -D -w 2 -b 0.0.0.0:3000 'src.server:app' --log-file=/dev/shm/gunicorn.log
+touch /tmp/gunicorn.log
+gunicorn --worker-tmp-dir /dev/shm -D --log-file=/tmp/gunicorn.log -w 2 -b 0.0.0.0:3000 src.server:app
 python -m src.main 2>&1 | tee /dev/null &
-tail -f /dev/shm/gunicorn.log
+exec tail -f /tmp/gunicorn.log
