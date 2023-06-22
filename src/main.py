5fbbc187efb7021f827da915f623ea67649a41ed
Jacob McSwain <jacob@mcswain.dev>
2023-06-22 01:01:05 -0500

    main: cleanup

--- src/main.py
+++ src/main.py
@@ -5,9 +5,7 @@ from .api import get_wx_watcher_manager
 
 
 def main():
-    # Trigger wx watcher initialization
     get_wx_watcher_manager()
-    # print(wx.get_alerts())
 
 
 if __name__ == '__main__':
